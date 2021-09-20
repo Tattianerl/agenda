c5ab1a5cf1565b04ee13e1f104e9cc9e82e6b692
Tattianerl <tattiane85@hotmail.com>
2021-09-20 17:10:36 -0300

    criado tabela de eventos para trabalhar com a agenda

--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao = models.DateTimeField(auto_now=True)#configura data atual do usuario
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)#se for esxcluir usuario,exclui tudo relacionado a ele
+
+    class Meta:
+        db_table = 'evento'
+
+    def __str__(self):
+        return self.titulo
+
+
+
+
